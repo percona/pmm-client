2fe7242087a5ecc66078a8ed7eb2488e7e90ecd7
Kamil Dziedzic <arvenil@klecza.pl>
2018-03-27 20:46:21 +0200

    PMM-2310: Change PMM client version.

--- pmm/constants.go
+++ pmm/constants.go
@@ -33,7 +33,7 @@ const (
 var (
 	// you can use `-ldflags -X github.com/percona/pmm-client/pmm.Version=`
 	// to set build version number
-	Version = "EXPERIMENTAL"
+	Version = "1.9.0"
 
 	// you can use `-ldflags -X github.com/percona/pmm-client/pmm.RootDir=`
 	// to set root filesystem for pmm-admin
