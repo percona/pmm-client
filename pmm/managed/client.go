c0ce97776f42f64f92076fe00fd1b95247d015d0
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2018-04-10 18:10:39 +0300

    PMM-2348 Added anonation command.

--- pmm/managed/client.go
+++ pmm/managed/client.go
@@ -167,3 +167,7 @@ func (c *Client) ScrapeConfigsUpdate(ctx context.Context, req *APIScrapeConfigsU
 func (c *Client) ScrapeConfigsDelete(ctx context.Context, jobName string) error {
 	return c.do(ctx, "DELETE", "/v0/scrape-configs/"+jobName, nil, nil)
 }
+
+func (c *Client) AnnotationCreate(ctx context.Context, req *APIAnnotationCreateRequest) error {
+	return c.do(ctx, "POST", "/v0/annotations", req, nil)
+}
