1d08d3c690bb8d79fe722e995233d95df0388805
Kamil Dziedzic <arvenil@klecza.pl>
2018-09-01 18:34:50 +0200

    PMM-2892: Add tests for `pmm-admin add {mysql,postgresql,mongodb}:metrics`.

--- pmm-admin_test.go
+++ pmm-admin_test.go
@@ -92,13 +92,16 @@ func TestPmmAdmin(t *testing.T) {
 	}
 	tests := []func(*testing.T, pmmAdminData){
 		testAddMongoDB,
+		testAddMongoDBMetrics,
+		testAddMongoDBQueries,
 		testAddPostgreSQL,
+		testAddPostgreSQLMetrics,
 		testAddPostgreSQLWithCreateUser,
 		testAddMySQL,
+		testAddMySQLMetrics,
 		testAddMySQLWithCreateUser,
 		testAddMySQLWithDisableSlowLogsRotation,
 		testAddMySQLWithRetainSlowLogs,
-		testAddMongoDBQueries,
 		testAddLinuxMetricsWithAdditionalArgsOk,
 		testAddLinuxMetricsWithAdditionalArgsFail,
 		testCheckNetwork,
@@ -1163,6 +1166,55 @@ func testAddPostgreSQL(t *testing.T, data pmmAdminData) {
 	assertRegexpLines(t, expected, string(output))
 }
 
+func testAddPostgreSQLMetrics(t *testing.T, data pmmAdminData) {
+	defer func() {
+		err := os.RemoveAll(data.rootDir)
+		assert.Nil(t, err)
+	}()
+	createFakeENV(t, data)
+
+	{
+		// Create fake api server
+		fapi := fakeapi.New()
+		fapi.AppendRoot()
+		fapi.AppendQanAPIPing()
+		fapi.AppendConsulV1StatusLeader()
+		node := api.CatalogNode{
+			Node: &api.Node{},
+		}
+		clientName, _ := os.Hostname()
+		fapi.AppendConsulV1CatalogNode(clientName, node)
+		fapi.AppendConsulV1CatalogService()
+		fapi.AppendConsulV1CatalogRegister()
+		fapi.AppendConsulV1KV()
+		_, host, port := fapi.Start()
+		defer fapi.Close()
+
+		// Configure pmm
+		cmd := exec.Command(
+			data.bin,
+			"config",
+			"--server",
+			fmt.Sprintf("%s:%s", host, port),
+		)
+		output, err := cmd.CombinedOutput()
+		assert.Nil(t, err, string(output))
+	}
+
+	cmd := exec.Command(
+		data.bin,
+		"add",
+		"postgresql:metrics",
+		"--user", "root",
+	)
+
+	output, err := cmd.CombinedOutput()
+	assert.Nil(t, err)
+	expected := `OK, now monitoring PostgreSQL metrics using DSN postgresql:\*\*\*@/postgres
+`
+	assertRegexpLines(t, expected, string(output))
+}
+
 func testAddPostgreSQLWithCreateUser(t *testing.T, data pmmAdminData) {
 	defer func() {
 		err := os.RemoveAll(data.rootDir)
@@ -1277,6 +1329,70 @@ func testAddMySQL(t *testing.T, data pmmAdminData) {
 	expected := `\[linux:metrics\] OK, now monitoring this system.
 \[mysql:metrics\] OK, now monitoring MySQL metrics using DSN root:\*\*\*@tcp\(127.0.0.1:3306\)
 \[mysql:queries\] OK, now monitoring MySQL queries from perfschema using DSN root:\*\*\*@tcp\(127.0.0.1:3306\)
+`
+	assertRegexpLines(t, expected, string(output))
+}
+
+func testAddMySQLMetrics(t *testing.T, data pmmAdminData) {
+	defer func() {
+		err := os.RemoveAll(data.rootDir)
+		assert.Nil(t, err)
+	}()
+	createFakeENV(t, data)
+
+	{
+		// Create fake api server
+		fapi := fakeapi.New()
+		fapi.AppendRoot()
+		fapi.AppendQanAPIPing()
+		fapi.AppendConsulV1StatusLeader()
+		node := api.CatalogNode{
+			Node: &api.Node{},
+		}
+		clientName, _ := os.Hostname()
+		fapi.AppendConsulV1CatalogNode(clientName, node)
+		fapi.AppendConsulV1CatalogService()
+		fapi.AppendConsulV1CatalogRegister()
+		fapi.AppendConsulV1KV()
+		in := &proto.Instance{
+			Subsystem: "mysql",
+			UUID:      "13",
+		}
+		agentInstance := &proto.Instance{
+			Subsystem: "agent",
+			UUID:      "42",
+		}
+		fapi.AppendQanAPIInstancesId(agentInstance.UUID, agentInstance)
+		fapi.AppendQanAPIAgents(agentInstance.UUID)
+		fapi.AppendQanAPIInstances([]*proto.Instance{
+			in,
+		})
+		_, host, port := fapi.Start()
+		defer fapi.Close()
+
+		// Configure pmm
+		cmd := exec.Command(
+			data.bin,
+			"config",
+			"--server",
+			fmt.Sprintf("%s:%s", host, port),
+		)
+		output, err := cmd.CombinedOutput()
+		assert.Nil(t, err, string(output))
+	}
+
+	cmd := exec.Command(
+		data.bin,
+		"add",
+		"mysql:metrics",
+		"--user", "root",
+		"--port", "3306", // MySQL instance with performance_schema enabled.
+		"--host", "127.0.0.1", // Force pmm-admin to ignore auto detection, otherwise it tries to connect to socket.
+	)
+
+	output, err := cmd.CombinedOutput()
+	assert.Nil(t, err)
+	expected := `OK, now monitoring MySQL metrics using DSN root:\*\*\*@tcp\(127.0.0.1:3306\)
 `
 	assertRegexpLines(t, expected, string(output))
 }
@@ -1643,6 +1759,67 @@ func testAddMongoDB(t *testing.T, data pmmAdminData) {
 \[mongodb:queries\] It is required for correct operation that profiling of monitored MongoDB databases be enabled.
 \[mongodb:queries\] Note that profiling is not enabled by default because it may reduce the performance of your MongoDB server.
 \[mongodb:queries\] For more information read PMM documentation \(https://www.percona.com/doc/percona-monitoring-and-management/conf-mongodb.html\).
+`
+	assertRegexpLines(t, expected, string(output))
+}
+
+func testAddMongoDBMetrics(t *testing.T, data pmmAdminData) {
+	defer func() {
+		err := os.RemoveAll(data.rootDir)
+		assert.Nil(t, err)
+	}()
+	createFakeENV(t, data)
+
+	{
+		// Create fake api server
+		fapi := fakeapi.New()
+		fapi.AppendRoot()
+		fapi.AppendQanAPIPing()
+		fapi.AppendConsulV1StatusLeader()
+		node := api.CatalogNode{
+			Node: &api.Node{},
+		}
+		clientName, _ := os.Hostname()
+		fapi.AppendConsulV1CatalogNode(clientName, node)
+		fapi.AppendConsulV1CatalogService()
+		fapi.AppendConsulV1CatalogRegister()
+		fapi.AppendConsulV1KV()
+		mongodbInstance := &proto.Instance{
+			Subsystem: "mongodb",
+			UUID:      "13",
+		}
+		agentInstance := &proto.Instance{
+			Subsystem: "agent",
+			UUID:      "42",
+		}
+		fapi.AppendQanAPIInstancesId(agentInstance.UUID, agentInstance)
+		fapi.AppendQanAPIAgents(agentInstance.UUID)
+		fapi.AppendQanAPIInstances([]*proto.Instance{
+			mongodbInstance,
+		})
+		_, host, port := fapi.Start()
+		defer fapi.Close()
+
+		// Configure pmm
+		cmd := exec.Command(
+			data.bin,
+			"config",
+			"--server",
+			fmt.Sprintf("%s:%s", host, port),
+		)
+		output, err := cmd.CombinedOutput()
+		assert.Nil(t, err, string(output))
+	}
+
+	cmd := exec.Command(
+		data.bin,
+		"add",
+		"mongodb:metrics",
+	)
+
+	output, err := cmd.CombinedOutput()
+	assert.Nil(t, err)
+	expected := `OK, now monitoring MongoDB metrics using URI localhost:27017
 `
 	assertRegexpLines(t, expected, string(output))
 }
