05b869c019ae15ccd535e0510245c24878639bbf
Vadim Yalovets <vadim.yalovets@percona.com>
2018-09-11 11:04:30 +0300

    PMM-2860 Improve pmm-client Diagnostics for Support

--- pmm/summary.go
+++ pmm/summary.go
@@ -29,7 +29,6 @@ import (
 	"path/filepath"
 )
 
-// Service status description.
 type Collector struct {
         CollectorDescription	string
 	ExecCommand		[]string
@@ -96,49 +95,39 @@ func zipIt(source, target string) error {
 		return err
 	}
 	defer zipfile.Close()
-
 	archive := zip.NewWriter(zipfile)
 	defer archive.Close()
-
 	info, err := os.Stat(source)
 	if err != nil {
 		return nil
 	}
-
 	var baseDir string
 	if info.IsDir() {
 		baseDir = filepath.Base(source)
 	}
-
 	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
 		header, err := zip.FileInfoHeader(info)
 		if err != nil {
 			return err
 		}
-
 		if baseDir != "" {
 			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
 		}
-
 		if info.IsDir() {
 			header.Name += "/"
 		} else {
 			header.Method = zip.Deflate
 		}
-
 		writer, err := archive.CreateHeader(header)
 		if err != nil {
 			return err
 		}
-
 		if info.IsDir() {
 			return nil
 		}
-
 		file, err := os.Open(path)
 		if err != nil {
 			return err
@@ -147,7 +136,6 @@ func zipIt(source, target string) error {
 		_, err = io.Copy(writer, file)
 		return err
 	})
-
 	return err
 }
 
